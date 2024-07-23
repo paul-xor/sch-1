ab03d54e934522cb7f46dbc9d1d49051c0fb66bd
Pasha <psmolov@nautilusdigital.com>
2024-07-22 20:40:07 -0400

    feat: requests validation

--- libs/contracts/src/lib/account/account.login.ts
+++ libs/contracts/src/lib/account/account.login.ts
@@ -1,9 +1,14 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export namespace AccountLogin {
   // how to name topics: service_name.command_name.type (last: command, even, query)
   export const topic = 'account.login.command'
 
   export class Request {
+    @IsEmail()
     email: string;
+
+    @IsString()
     password: string;
   }
 
--- libs/contracts/src/lib/account/account.register.ts
+++ libs/contracts/src/lib/account/account.register.ts
@@ -1,10 +1,18 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
 export namespace AccountRegister {
   // how to name topics: service_name.command_name.type (last: command, even, query)
   export const topic = 'account.register.command'
 
   export class Request {
+    @IsEmail()
     email: string;
+
+    @IsString()
     password: string;
+
+    @IsOptional()
+    @IsString()
     displayName?: string;
   }
 
